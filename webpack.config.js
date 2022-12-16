8d87b4414b7e3cb1d4da63fd3fd7a442f418047e
ULADZISLAVA <vladislava96.96@mail.ru>
2022-12-16 17:53:31 +0300

    feat: add bootstrap and sass

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ const baseConfig = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.tsx?$/,

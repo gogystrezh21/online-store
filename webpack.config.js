c4961d5d953657ee87c988ac722e4107dc5c8878
ULADZISLAVA <vladislava96.96@mail.ru>
2022-12-20 14:24:54 +0300

    fix: eslint rules

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const EslingPlugin = require('eslint-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
@@ -38,8 +38,8 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
-        new EslingPlugin({ extensions: 'ts' }),
-        new CopyPlugin({ patterns: [{ from: './src/assets', to: './assets' }], })
+        new EslintPlugin({ extensions: 'ts' }),
+        new CopyPlugin({ patterns: [{ from: './src/assets', to: './assets' }] }),
     ],
 };
 
@@ -49,4 +49,3 @@ module.exports = ({ mode }) => {
 
     return merge(baseConfig, envConfig);
 };
-
